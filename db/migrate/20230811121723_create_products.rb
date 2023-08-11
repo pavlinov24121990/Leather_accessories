56c654d51c594ebf60abaec0fac08a4d9e0b1e8d
Aleksey <pavlinov24121990@gmail.com>
2023-08-11 14:20:25 +0200

    install_config_info_and_index_v3

--- db/migrate/20230811121723_create_products.rb
+++ db/migrate/20230811121723_create_products.rb
@@ -2,8 +2,9 @@ class CreateProducts < ActiveRecord::Migration[7.0]
   def change
     create_table :products do |t|
       t.string :title
-      t.text :body
+      t.text :description
       t.float :price
+      t.belongs_to :category, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_11_082844) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_11_121723) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,11 +22,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_11_082844) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
-    t.text "body"
+    t.text "description"
     t.float "price"
+    t.bigint "category_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "category_id", null: false
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
