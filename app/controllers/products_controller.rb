5fe857158f79b3a8f9c2ff267554a0868ced7e47
Aleksey <pavlinov24121990@gmail.com>
2023-09-06 14:15:38 +0200

    install_show_product_user_V1

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,8 +2,20 @@ class ProductsController < ApplicationController
 
   include Pagy::Backend
 
+  before_action :product_find, only: %i[show]
+
   def index
      @pagy, @products = pagy(Product.order(created_at: :asc), items: 6)
   end
+
+  def show
+    
+  end
   
+  private
+
+  def product_find
+    @product = Product.find(params[:id])
+  end
+
 end
