2decd9d0e7cb9f5abf357301d3b015994aad21ab
Aleksey <pavlinov24121990@gmail.com>
2023-09-07 10:57:19 +0200

    Create_Cart_and_CartItem_model_finish_V3

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -2,6 +2,6 @@ class CartItem < ApplicationRecord
   belongs_to :cart
   belongs_to :product
 
-  validates :quantity, presence: true, numericality: { less_than_or_equal_to: 5 }
+  validates :quantity, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 
 end
